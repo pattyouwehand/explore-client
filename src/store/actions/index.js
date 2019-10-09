2d1a43b343799aeb3dc1022d7925f1e625c1d7e1
pattyouwehand <pattyouwehand@gmail.com>
2019-10-09 12:46:21 +0200

    youtube library loaded

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,23 +1,6 @@
-import request from 'superagent'
-export const ALL_IMAGES = 'ALL_VIDEOS'
-const baseUrl = 'http://localhost:4000'
-
-function allImages (payload) {
+export function createAction(type, payload = {}) {
   return {
-    type: ALL_VIDEOS,
-    payload
-  }
-}
-export const getVideos = () => (dispatch, getState) => {
-  const state = getState()
-  const { videos } = state
-
-  if (!videos.length) {
-    request(`${baseUrl}/videos`)
-      .then(response => {
-        const action = allVideos(response.body)
-        dispatch(action)
-      })
-      .catch(console.error)
+    type,
+    ...payload,
   }
 }
