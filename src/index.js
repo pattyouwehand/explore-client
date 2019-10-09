9e44cf74d868d3621864a30e24029eb9b352e69e
pattyouwehand <pattyouwehand@gmail.com>
2019-10-09 11:49:19 +0200

    modify store and add redux saga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as serviceWorker from './serviceWorker';
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
+import { Provider } from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+import {configureStore} from './store/configureStore'
+
+const store = configureStore();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'))
 
-serviceWorker.unregister();
