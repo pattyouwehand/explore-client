bffa753ee8262ee8aab15649c67ef9f677cba6dc
pattyouwehand <pattyouwehand@gmail.com>
2019-10-09 11:23:48 +0200

    watch component added metadata, likes, description and comments

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './Watch.scss'
-import {VideoPreview} from '../../components/VideoPreview/VideoPreview'
+import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos'
+import {Video} from '../../components/Video/Video'
+import {VideoMetadata} from '../../components/VideoMetadata/VideoMetadata'
+import {VideoInfoBox} from '../../components/VideoInfoBox/VideoInfoBox'
+import {Comments} from '../Comments/Comments'
 
 export class Watch extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
-        <VideoPreview horizontal={true} />
-        <VideoPreview />
-      </React.Fragment>
-    );
+      <div className='watch-grid'>
+        <Video className='video' id='1zzYZCQ5PCQ' />
+        <VideoMetadata className='metadata' viewCount={1000} />
+        <VideoInfoBox className='video-info-box' />
+        <Comments className='comments' />
+        <RelatedVideos className='relatedVideos'/>
+      </div>
+    )
   }
 }
